e9f844f758529631fed83f110162cf2d0f54f957
bagaas86 <bagaas86@gmail.com>
2023-10-23 21:41:19 +0700

    Fixing Bug landing page

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -18,22 +18,19 @@
                                             <div class="input-group input-group-outline mt-3">
                                                 <label class="form-label">Username</label>
                                                 <input type="text" class="form-control" name="username"
-<<<<<<< HEAD
                                                     value="{{ old('username') }}">
-=======
-                                                    value="{{ old('username') }}" required>
                                             </div>
                                             @error('username')
-                                            <p class='text-danger inputerror'>{{ $message }} </p>
+                                                <p class='text-danger inputerror'>{{ $message }} </p>
                                             @enderror
                                             <div class="input-group input-group-outline mt-3">
                                                 <label class="form-label">Name</label>
                                                 <input type="text" class="form-control" name="name"
                                                     value="{{ old('name') }}" required>
->>>>>>> bd24631c99becb8d5470cb238fd2c9e78b0544b6
+
                                             </div>
                                             @error('username')
-                                            <p class='text-danger inputerror'>{{ $message }} </p>
+                                                <p class='text-danger inputerror'>{{ $message }} </p>
                                             @enderror
                                             <div class="input-group input-group-outline mt-3">
                                                 <label class="form-label">Nama</label>
@@ -41,7 +38,7 @@
                                                     value="{{ old('nama') }}">
                                             </div>
                                             @error('nama')
-                                            <p class='text-danger inputerror'>{{ $message }} </p>
+                                                <p class='text-danger inputerror'>{{ $message }} </p>
                                             @enderror
                                             <div class="input-group input-group-outline mt-3">
                                                 <label class="form-label">Email</label>
@@ -49,25 +46,22 @@
                                                     value="{{ old('email') }}" required>
                                             </div>
                                             @error('email')
-                                            <p class='text-danger inputerror'>{{ $message }} </p>
+                                                <p class='text-danger inputerror'>{{ $message }} </p>
                                             @enderror
                                             <div class="input-group input-group-outline mt-3">
                                                 <label class="form-label">Password</label>
                                                 <input type="password" class="form-control" name="password" required>
                                             </div>
                                             @error('password')
-                                            <p class='text-danger inputerror'>{{ $message }} </p>
+                                                <p class='text-danger inputerror'>{{ $message }} </p>
                                             @enderror
                                             <div class="input-group input-group-outline mt-3">
-<<<<<<< HEAD
-                                                <label class="form-label">Konfirmasi Password</label>
-                                                <input type="password" class="form-control" name="kpassword">
-=======
                                                 <label class="form-label">Kontak</label>
-                                                <input type="test" class="form-control" name="kontak" value="{{ old('kontak') }}" required>
+                                                <input type="test" class="form-control" name="kontak"
+                                                    value="{{ old('kontak') }}" required>
                                             </div>
                                             <div class="input-group input-group-outline mt-3">
-                                                <select name="role"  class="form-control" required>
+                                                <select name="role" class="form-control" required>
                                                     <option selected disabled>Role</option>
                                                     <option value="okp">Organisasi Kepemudaan</option>
                                                     <option value="wm">Wirausaha Muda</option>
@@ -82,10 +76,9 @@
                                                     I agree the <a href="javascript:;"
                                                         class="text-dark font-weight-bolder">Terms and Conditions</a>
                                                 </label>
->>>>>>> bd24631c99becb8d5470cb238fd2c9e78b0544b6
                                             </div>
                                             @error('kpassword')
-                                            <p class='text-danger inputerror'>{{ $message }} </p>
+                                                <p class='text-danger inputerror'>{{ $message }} </p>
                                             @enderror
                                             <div class="input-group input-group-outline mt-3">
                                                 <label class="form-label">No Hp</label>
@@ -93,11 +86,10 @@
                                                     value="{{ old('kontak') }}">
                                             </div>
                                             @error('kontak')
-                                            <p class='text-danger inputerror'>{{ $message }} </p>
+                                                <p class='text-danger inputerror'>{{ $message }} </p>
                                             @enderror
                                             <div class="input-group input-group-outline mb-3 mt-3">
-                                                <label class="form-label">Jenis</label>
-                                                <select class="form-select" name="role" required>
+                                                <select class="form-control" name="role" required>
                                                     <option value="">Pilih jenis...</option>
                                                     <option value="Pemuda Pelopor">Pemuda Pelopor</option>
                                                     <option value="Wirausaha Muda">Wirausaha Muda</option>
@@ -108,25 +100,31 @@
 
 
                                             @error('kontak')
-                                            <p class='text-danger inputerror'>{{ $message }} </p>
+                                                <p class='text-danger inputerror'>{{ $message }} </p>
                                             @enderror
                                             <div class="text-center">
-                                                <button type="submit"
-                                                    class="btn btn-lg btn-lg w-100 mt-4 mb-0" style="background-color: #0057FF;"><i class="text-sm text-white" >Sign Up</i>
-                                                    </button>
+                                                <button type="submit" class="btn btn-lg btn-lg w-100 mt-4 mb-0"
+                                                    style="background-color: #0057FF;"><i
+                                                        class="text-sm text-white">Sign Up</i>
+                                                </button>
                                             </div>
-                                            <div class="text-center">
-                                                <button type="close"
-                                                    class="btn btn-lg bg- btn-lg w-100 mt-4 mb-0" style="background-color: #000000;"><i class="text-sm text-white" >Kembali Ke Login</i></button>
-                                            </div>
+
                                         </form>
+                                        <div class="text-center">
+                                            <a href="{{ url('sign-in') }}"
+                                                class="btn btn-lg bg- btn-lg w-100 mt-4 mb-0"
+                                                style="background-color: #000000;"><i class="text-sm text-white">Kembali
+                                                    Ke Login</i></a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <div
                                 class="col-4 d-lg-flex d-none my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-end flex-column">
-                                <div class="position-relative   border-radius-lg d-flex flex-column justify-content-center">
-                                    <img src="{{ asset('assets/img/bg_in.png') }}" alt="profile-img" class="text-center" />
+                                <div
+                                    class="position-relative   border-radius-lg d-flex flex-column justify-content-center">
+                                    <img src="{{ asset('assets/img/bg_in.png') }}" alt="profile-img"
+                                        class="text-center" />
                                 </div>
                             </div>
                         </div>
@@ -137,17 +135,17 @@
     </div>
 
     @push('js')
-    <script src="{{ asset('assets') }}/js/jquery.min.js"></script>
-    <script>
-        $(function() {
+        <script src="{{ asset('assets') }}/js/jquery.min.js"></script>
+        <script>
+            $(function() {
 
-        var text_val = $(".input-group input").val();
-        if (text_val === "") {
-          $(".input-group").removeClass('is-filled');
-        } else {
-          $(".input-group").addClass('is-filled');
-        }
-    });
-    </script>
+                var text_val = $(".input-group input").val();
+                if (text_val === "") {
+                    $(".input-group").removeClass('is-filled');
+                } else {
+                    $(".input-group").addClass('is-filled');
+                }
+            });
+        </script>
     @endpush
 </x-layout>
