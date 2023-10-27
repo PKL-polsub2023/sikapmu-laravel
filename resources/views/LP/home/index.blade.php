b9cde3157a58b8a19a931b41f98d32cd79e44f45
gusnanda7 <gus11111234@gmail.com>
2023-10-27 10:26:15 +0700

    comit

--- resources/views/LP/home/index.blade.php
+++ resources/views/LP/home/index.blade.php
@@ -64,18 +64,28 @@
     <section class="services service-2 ptb-100 bg-light" data-aos="fade-down">
         <div class="container" data-aos="flip-up">
             <div class="default-section-title default-section-title-middle">
-                <h3 class="text-black">Evaluasi Data Kependudukan</h3>
+                <h3 class="text-black">Evaluasi Data Kepemudan</h3>
             </div>
+            @php
+            $sekarang = date('Y');
+            @endphp
+            <center>
+             <div class="col col-md-1 mt-3" style="height: 50px">
+                <select id="tahun" class="form-control" onchange="chart()" style="text-align: center;">
+                    @foreach ($dp as $item)
+                        <option @if ($item->tahun == $sekarang) selected @endif value="{{ $item->tahun }}">
+                            {{ $item->tahun }}</option>
+                    @endforeach
+                </select>
+            </div>
+            </center>
             <div class="section-content">
                 <div class="row mt-5">
                     <div class="col-md-4">
                         <div class="card card-white-box">
                             <div class="p-2">
                             <div id="pie1" style="min-width: 310px; height: 500px; max-width: 100%; margin: 0 auto"></div>
-                                <div class="d-flex justify-content-center py-2">
-                                    <a href="/" class="default-button news-btn">Detail</a>
-                                    <h5 class="ms-3 mt-2"><i class="fas fa-user"></i> 100</h5>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
@@ -83,10 +93,7 @@
                         <div class="card card-white-box">
                             <div class="p-2">
                             <div id="pie2" style="min-width: 310px; height: 500px; max-width: 100%; margin: 0 auto"></div>
-                                <div class="d-flex justify-content-center py-2">
-                                    <a href="/" class="default-button news-btn">Detail</a>
-                                    <h5 class="ms-3 mt-2"><i class="fas fa-user"></i> 100</h5>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
@@ -94,17 +101,60 @@
                         <div class="card card-white-box">
                             <div class="p-2">
                             <div id="pie3" style="min-width: 310px; height: 500px; max-width: 100%; margin: 0 auto"></div>
-                                <div class="d-flex justify-content-center py-2">
-                                    <a href="/" class="default-button news-btn">Detail</a>
-                                    <h5 class="ms-3 mt-2"><i class="fas fa-user"></i> 100</h5>
-                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-white-box">
+                            <div class="p-2">
+                            <div id="pie4" style="min-width: 310px; height: 500px; max-width: 100%; margin: 0 auto"></div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-white-box">
+                            <div class="p-2">
+                            <div id="pie5" style="min-width: 310px; height: 500px; max-width: 100%; margin: 0 auto"></div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-white-box">
+                            <div class="p-2">
+                            <div id="pie6" style="min-width: 310px; height: 500px; max-width: 100%; margin: 0 auto"></div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-white-box">
+                            <div class="p-2">
+                            <div id="pie7" style="min-width: 310px; height: 500px; max-width: 100%; margin: 0 auto"></div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-white-box">
+                            <div class="p-2">
+                            <div id="pie8" style="min-width: 310px; height: 500px; max-width: 100%; margin: 0 auto"></div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-white-box">
+                            <div class="p-2">
+                            <div id="pie9" style="min-width: 310px; height: 500px; max-width: 100%; margin: 0 auto"></div>
+                                
                             </div>
                         </div>
                     </div>
                     <div class="col-md-12">
-                    <center>
-                    <a href="/" class="default-button news-btn mt-5">Selengkapnya</a>
-                    </center>
+                   
                     <div class="card card-white-box mt-5">
                         <div class="p-2">
                             <div class="d-flex justify-content-between py-4">
@@ -567,4 +617,382 @@ Highcharts.chart('pie3', {
     }]
 });
 </script>
+<script type="text/javascript">
+    // Build the chart
+    Highcharts.chart('pie4', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: '<h2>Data Kewirausahawan pemuda</h2>'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: {point.y} : {point.percentage:.1f}%'
+        },
+        legend: {
+            layout: 'horizontal',
+            backgroundColor: '',
+            floating: true,
+            align: 'left',
+            verticalAlign: 'bottom',
+            x: 0,
+            y: 0,
+            labelFormatter: function () {
+                return this.name + ' '+this.options.y+' orang';
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    // format: '<b>{point.name}</b> : {point.y} orang',
+                    // style: {
+                    // color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    // }
+                },
+                showInLegend: true,
+    
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Utara',
+                y: 360,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Selatan',
+                y: 320
+            }, {
+                name: 'Timur',
+                y: 260
+            }, {
+                name: 'Barat',
+                y: 60
+            }]
+        }]
+    });
+    </script>
+<script type="text/javascript">
+    // Build the chart
+    Highcharts.chart('pie5', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: '<h2>Data Kewirausahawan pemuda</h2>'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: {point.y} : {point.percentage:.1f}%'
+        },
+        legend: {
+            layout: 'horizontal',
+            backgroundColor: '',
+            floating: true,
+            align: 'left',
+            verticalAlign: 'bottom',
+            x: 0,
+            y: 0,
+            labelFormatter: function () {
+                return this.name + ' '+this.options.y+' orang';
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    // format: '<b>{point.name}</b> : {point.y} orang',
+                    // style: {
+                    // color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    // }
+                },
+                showInLegend: true,
+    
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Utara',
+                y: 360,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Selatan',
+                y: 320
+            }, {
+                name: 'Timur',
+                y: 260
+            }, {
+                name: 'Barat',
+                y: 60
+            }]
+        }]
+    });
+    </script>
+<script type="text/javascript">
+    // Build the chart
+    Highcharts.chart('pie6', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: '<h2>Data Organisasi Kepemudaan</h2>'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: {point.y} : {point.percentage:.1f}%'
+        },
+        legend: {
+            layout: 'horizontal',
+            backgroundColor: '',
+            floating: true,
+            align: 'left',
+            verticalAlign: 'bottom',
+            x: 0,
+            y: 0,
+            labelFormatter: function () {
+                return this.name + ' '+this.options.y+' orang';
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    // format: '<b>{point.name}</b> : {point.y} orang',
+                    // style: {
+                    // color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    // }
+                },
+                showInLegend: true,
+    
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Utara',
+                y: 360,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Selatan',
+                y: 320
+            }, {
+                name: 'Timur',
+                y: 260
+            }, {
+                name: 'Barat',
+                y: 60
+            }]
+        }]
+    });
+    </script>
+    <script type="text/javascript">
+        // Build the chart
+        Highcharts.chart('pie7', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: '<h2>Data Organisasi Kepemudaan</h2>'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: {point.y} : {point.percentage:.1f}%'
+            },
+            legend: {
+                layout: 'horizontal',
+                backgroundColor: '',
+                floating: true,
+                align: 'left',
+                verticalAlign: 'bottom',
+                x: 0,
+                y: 0,
+                labelFormatter: function () {
+                    return this.name + ' '+this.options.y+' orang';
+                }
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        // format: '<b>{point.name}</b> : {point.y} orang',
+                        // style: {
+                        // color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        // }
+                    },
+                    showInLegend: true,
+        
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: [{
+                    name: 'Utara',
+                    y: 360,
+                    sliced: true,
+                    selected: true
+                }, {
+                    name: 'Selatan',
+                    y: 320
+                }, {
+                    name: 'Timur',
+                    y: 260
+                }, {
+                    name: 'Barat',
+                    y: 60
+                }]
+            }]
+        });
+        </script>
+        <script type="text/javascript">
+            // Build the chart
+            Highcharts.chart('pie8', {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: '<h2>Data Organisasi Kepemudaan</h2>'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: {point.y} : {point.percentage:.1f}%'
+                },
+                legend: {
+                    layout: 'horizontal',
+                    backgroundColor: '',
+                    floating: true,
+                    align: 'left',
+                    verticalAlign: 'bottom',
+                    x: 0,
+                    y: 0,
+                    labelFormatter: function () {
+                        return this.name + ' '+this.options.y+' orang';
+                    }
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            // format: '<b>{point.name}</b> : {point.y} orang',
+                            // style: {
+                            // color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                            // }
+                        },
+                        showInLegend: true,
+            
+                    }
+                },
+                series: [{
+                    name: 'Brands',
+                    colorByPoint: true,
+                    data: [{
+                        name: 'Utara',
+                        y: 360,
+                        sliced: true,
+                        selected: true
+                    }, {
+                        name: 'Selatan',
+                        y: 320
+                    }, {
+                        name: 'Timur',
+                        y: 260
+                    }, {
+                        name: 'Barat',
+                        y: 60
+                    }]
+                }]
+            });
+            </script>
+            <script type="text/javascript">
+                // Build the chart
+                Highcharts.chart('pie9', {
+                    chart: {
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false,
+                        type: 'pie'
+                    },
+                    title: {
+                        text: '<h2>Data Organisasi Kepemudaan</h2>'
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: {point.y} : {point.percentage:.1f}%'
+                    },
+                    legend: {
+                        layout: 'horizontal',
+                        backgroundColor: '',
+                        floating: true,
+                        align: 'left',
+                        verticalAlign: 'bottom',
+                        x: 0,
+                        y: 0,
+                        labelFormatter: function () {
+                            return this.name + ' '+this.options.y+' orang';
+                        }
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: true,
+                                // format: '<b>{point.name}</b> : {point.y} orang',
+                                // style: {
+                                // color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                                // }
+                            },
+                            showInLegend: true,
+                
+                        }
+                    },
+                    series: [{
+                        name: 'Brands',
+                        colorByPoint: true,
+                        data: [{
+                            name: 'Utara',
+                            y: 360,
+                            sliced: true,
+                            selected: true
+                        }, {
+                            name: 'Selatan',
+                            y: 320
+                        }, {
+                            name: 'Timur',
+                            y: 260
+                        }, {
+                            name: 'Barat',
+                            y: 60
+                        }]
+                    }]
+                });
+                </script>
 @endsection
