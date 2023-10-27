afe16e1c8220f628bfd8b416f66eb16fe1f6f3a0
gusnanda7 <gus11111234@gmail.com>
2023-10-27 13:57:28 +0700

    comit

--- resources/views/pages/laravel-examples/detailwm.blade.php
+++ resources/views/pages/laravel-examples/detailwm.blade.php
@@ -14,70 +14,14 @@
    </div>
 </section>
 @include('template_front.support')
-{{-- <section class="services service-2 ptb-100 bg-white" data-aos="fade-down">
-   <div class="container" data-aos="flip-up">
-      <div class="default-section-title default-section-title-middle">
-            <h3 class="text-black">Wirausaha Muda Subang</h3>
-      </div>
-      <div class="section-content">
-            <div class="row mt-5 justify-content-center">
-               <div class="col-md-6">
-                  <div class="card card-white-box">
-                        <div class="p-2">
-                        <div id="pie1" style="min-width: 310px; height: 400px; max-width: 100%; margin: 0 auto"></div>
-                          
-                        </div>
-                  </div>
-               </div>
-               <div class="col-md-3">
-                  <div class="card card-white-box">
-                     <div class="p-2">
-                        <h5 class="ms-3 mt-2 text-center"> Wirausaha Muda <br> {{ $j->wira_usaha_muda }}</h5>
-                     </div>
-                  </div>
-               </div>
-            </div>
-      </div>
-   </div>
-</section> --}}
+
 <section class="team ptb-100 bg-f9fbfe">
     <div class="container-fluid py-4">
-        <div class="col-3 text-start">
-            <h1 class="d-flex text-start align-items-start"
-                style="color: rgb(0, 0, 0); font-size: 24px; font-weight: bold; margin-right: 5px;">
-                Bio Wirausahawan
-            </h1>
-        </div>
-        <div class="card py-4 px-4" style="border-radius: 10px; background-color: #293679; position:relative;">
-            <div class="row">
-                <div class="col-2"></div>
-                <div class="col-3 mt-3">
-                    <h1 class="d-flex text-center align-items-center"
-                        style="color: rgb(255, 255, 255); font-size: 24px; font-weight: bold;">
-                        {{ $wirausaha->nama }}
-                    </h1>
-                    <h1 class="d-flex text-center align-items-center"
-                        style="color: rgb(255, 255, 255); font-size: 18px; font-weight: 50;">
-                        {{ $wirausaha->nama_wirausaha }}
-                    </h1>
-                    <h1 class="d-flex text-center align-items-center"
-                        style="color: rgb(255, 255, 255); font-size:18px; font-weight: 100;">{{ $wirausaha->ttl }}
-                    </h1>
-                </div>
-                <div class="col-7">
-                    
-                            <div class="container" style="max-width: 500px;">
-                                <h1 class="text-start mx-3 mb-4"
-                                    style="color: rgb(255, 255, 255); font-size: 18px; font-weight: 100; white-space: pre-line;">
-                                    Alamat : {{ $wirausaha->alamat }}
-                                </h1>
-                            </div>
-                        
-
-                </div>
-            </div>
-
-        </div>
+        <div class="default-section-title default-section-title-middle">
+            <h3>Wirausaha Muda Subang</h3>
+            <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua quis ipsum suspendisse</p> -->
+         </div>
+         <div class=""></div>
 
         <div class="col-2 d-flex justify-content-end mt-n7">
             <a class="nav-link text-start p-0" id="profile" style="margin-left: 500px;">
