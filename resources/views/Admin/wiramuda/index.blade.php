b0d028dadc05a27203b0b8fdabe2ab562aee49f4
gusnanda7 <gus11111234@gmail.com>
2023-10-30 08:46:00 +0700

    Sipanda->SIlapem

--- resources/views/Admin/wiramuda/index.blade.php
+++ resources/views/Admin/wiramuda/index.blade.php
@@ -156,7 +156,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="adddoc">Daftar SiPanda</h5>
+                <h5 class="modal-title" id="adddoc">Daftar SILAPEM</h5>
                 <button type="button" class="btn-close" id="closed" data-bs-dismiss="modal"
                     aria-label="Close"></button>
             </div>
@@ -214,7 +214,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="adddoc">Edit SiPanda</h5>
+                    <h5 class="modal-title" id="adddoc">Edit SILAPEM</h5>
                     <button type="button" id="closed2" class="btn-close" data-bs-dismiss="modal"
                         aria-label="Close"></button>
                 </div>
